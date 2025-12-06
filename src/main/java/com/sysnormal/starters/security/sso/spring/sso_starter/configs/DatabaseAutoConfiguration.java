3aa71a1926b60ed7a5cadd6955b9f8c1cb1ecb44
ti-jumbo <jumbo.ti@jumboalimentos.com.br>
2025-12-06 20:24:39 -0300

    adjusted string literals to constants

--- src/main/java/com/sysnormal/starters/security/sso/spring/sso_starter/configs/DatabaseAutoConfiguration.java
+++ src/main/java/com/sysnormal/starters/security/sso/spring/sso_starter/configs/DatabaseAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.sysnormal.starters.security.sso.spring.sso_starter.configs;
 import com.sysnormal.starters.security.sso.spring.sso_starter.database.entities.sso.User;
 import com.sysnormal.starters.security.sso.spring.sso_starter.database.repositories.sso.UsersRepository;
 import com.sysnormal.starters.security.sso.spring.sso_starter.properties.database.DatabaseProperties;
+import com.sysnormal.libs.utils.Constants;
 import jakarta.persistence.EntityManagerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,14 +33,20 @@ import java.util.Map;
 @Configuration(proxyBeanMethods = false)
 @EnableJpaRepositories(
         basePackageClasses = UsersRepository.class,
-        entityManagerFactoryRef = "ssoEntityManagerFactory",
-        transactionManagerRef = "ssoTransactionManager"
+        entityManagerFactoryRef = DatabaseAutoConfiguration.entityManagerFactoryQualifier,
+        transactionManagerRef = DatabaseAutoConfiguration.transactionManagerQualifier
 )
 @ConditionalOnProperty(prefix = "sso.database", name = "enabled", havingValue = "true", matchIfMissing = true)
 @EnableConfigurationProperties(DatabaseProperties.class)
 public class DatabaseAutoConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(DatabaseAutoConfiguration.class);
+
+    public static final String datasourceName = "sso";
+    public static final String datasourcePrefix = Constants.Spring.DATASOURCE.PROPERTY + "." + datasourceName;
+    public static final String persistenceUnitName = datasourceName;
+    public static final String entityManagerFactoryQualifier = datasourceName +  Constants.Jakarta.PERSISTENCE.ENTITYMANAGERFACTORY.CLASSNAME;
+    public static final String transactionManagerQualifier = datasourceName + Constants.SpringFramework.TRANSACTION.TRANSACTIONMANAGER.CLASSNAME;
 
     /**
      * properties
@@ -87,26 +94,26 @@ public class DatabaseAutoConfiguration {
      */
     @Bean
     @Primary
-    @ConditionalOnMissingBean(name = "ssoEntityManagerFactory")
+    @ConditionalOnMissingBean(name = entityManagerFactoryQualifier)
     public LocalContainerEntityManagerFactoryBean ssoEntityManagerFactory(EntityManagerFactoryBuilder builder) {
-        logger.debug("INIT {}.{}", this.getClass().getSimpleName(), "ssoEntityManagerFactory");
+        logger.debug("INIT {}.{}", this.getClass().getSimpleName(), entityManagerFactoryQualifier);
         LocalContainerEntityManagerFactoryBean result = null;
         try {
             Map<String, Object> jpaProps = new HashMap<>();
-            jpaProps.put("hibernate.hbm2ddl.auto", properties.getJpa().getHibernate().getDdlAuto());
-            jpaProps.put("hibernate.dialect", properties.getJpa().getHibernate().getDialect());
+            jpaProps.put(org.hibernate.cfg.AvailableSettings.HBM2DDL_AUTO, properties.getJpa().getHibernate().getDdlAuto());
+            jpaProps.put(Constants.Hibernate.DIALECT.PROPERTY, properties.getJpa().getHibernate().getDialect());
             //jpaProps.put("properties", properties.getJpa().getProperties());
-            jpaProps.put("hibernate.globally_quoted_identifiers", properties.getJpa().getHibernate().isGloballyQuotedIdentifiers());
+            jpaProps.put(Constants.Hibernate.GLOBALLY_QUOTED_IDENTIFIERS.PROPERTY, properties.getJpa().getHibernate().isGloballyQuotedIdentifiers());
             result = builder
                     .dataSource(ssoDataSource())
                     .packages(User.class)
-                    .persistenceUnit("sso")
+                    .persistenceUnit(persistenceUnitName)
                     .properties(jpaProps)
                     .build();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        logger.debug("END {}.{}", this.getClass().getSimpleName(), "ssoEntityManagerFactory");
+        logger.debug("END {}.{}", this.getClass().getSimpleName(), entityManagerFactoryQualifier);
         return result;
     }
 
@@ -118,16 +125,16 @@ public class DatabaseAutoConfiguration {
      */
     @Bean
     @Primary
-    @ConditionalOnMissingBean(name = "ssoTransactionManager")
-    public PlatformTransactionManager ssoTransactionManager(@Qualifier("ssoEntityManagerFactory") EntityManagerFactory ssoEntityManagerFactory) {
-        logger.debug("INIT {}.{}", this.getClass().getSimpleName(), "ssoTransactionManager");
+    @ConditionalOnMissingBean(name = transactionManagerQualifier)
+    public PlatformTransactionManager ssoTransactionManager(@Qualifier(entityManagerFactoryQualifier) EntityManagerFactory ssoEntityManagerFactory) {
+        logger.debug("INIT {}.{}", this.getClass().getSimpleName(), transactionManagerQualifier);
         JpaTransactionManager result = null;
         try {
             result = new JpaTransactionManager(ssoEntityManagerFactory);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        logger.debug("END {}.{}", this.getClass().getSimpleName(), "ssoTransactionManager");
+        logger.debug("END {}.{}", this.getClass().getSimpleName(), transactionManagerQualifier);
         return result;
     }
 }
